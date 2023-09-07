49efc84394ad7f7aef3fcb8c9b173ac248911722
Crmit-Mukesh-jain <mukesh.jain@crmit.com>
2023-09-07 17:39:45 +0530

    Changes of the Event and Marketing Tabs

--- js/script.js
+++ js/script.js
@@ -128,5 +128,150 @@ $(function() {
         $("#oAnnualRev3").hide();
       }
     });
+
+    $("#SpeakingOpportunity").click(function () {
+      if ($(this).is(":checked")) {
+          $("#SpeakingOpprtunityContent").show();
+      } else {
+          $("#SpeakingOpprtunityContent").hide();
+      }
+    });
+    $("#Ad1").click(function () {
+      if ($(this).is(":checked")) {
+          $("#AdContent1").show();
+      } else {
+          $("#AdContent1").hide();
+      }
+    });
+
+    $("#PromoItems").click(function () {
+      if ($(this).is(":checked")) {
+          $("#PromoItemsContent").show();
+      } else {
+          $("#PromoItemsContent").hide();
+      }
+    });
+
+    $("#Raffle").click(function () {
+      if ($(this).is(":checked")) {
+          $("#RaffleContent").show();
+      } else {
+          $("#RaffleContent").hide();
+      }
+    });
+
+    $("#ExhibitSpace").click(function () {
+      if ($(this).is(":checked")) {
+          $("#ExhibitSpaceContent").show();
+      } else {
+          $("#ExhibitSpaceContent").hide();
+      }
+    });
+
+    $("#StrategicDinner").click(function () {
+      if ($(this).is(":checked")) {
+          $("#StrategicDinnerContent").show();
+      } else {
+          $("#StrategicDinnerContent").hide();
+      }
+    });
+
+    $("#Others").click(function () {
+      if ($(this).is(":checked")) {
+          $("#OthersContent").show();
+      } else {
+          $("#OthersContent").hide();
+      }
+    });
+
+    // $('#checkall:checkbox').change(function () {
+    //   if($(this).attr("checked")) $('input:checkbox').attr('checked','checked');
+    //   else $('input:checkbox').removeAttr('checked');
+    // });
+
+    $("#add_row").on("click", function() {
+      // Dynamic Rows Code
+      
+      // Get max row id and set new id
+      var newid = 0;
+      $.each($("#tab_logic tr"), function() {
+          if (parseInt($(this).data("id")) > newid) {
+              newid = parseInt($(this).data("id"));
+          }
+      });
+      newid++;
+      
+      var tr = $("<tr></tr>", {
+          id: "addr"+newid,
+          "data-id": newid
+      });
+      
+      // loop through each td and create new elements with name of newid
+      $.each($("#tab_logic tbody tr:nth(0) td"), function() {
+          var td;
+          var cur_td = $(this);
+          
+          var children = cur_td.children();
+          
+          // add new td and element if it has a nane
+          if ($(this).data("name") !== undefined) {
+              td = $("<td></td>", {
+                  "data-name": $(cur_td).data("name")
+              });
+              
+              var c = $(cur_td).find($(children[0]).prop('tagName')).clone().val("");
+              c.attr("name", $(cur_td).data("name") + newid);
+              c.appendTo($(td));
+              td.appendTo($(tr));
+          } else {
+              td = $("<td></td>", {
+                  'text': $('#tab_logic tr').length
+              }).appendTo($(tr));
+          }
+      });
+      
+      // add delete button and td
+      /*
+      $("<td></td>").append(
+          $("<button class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>")
+              .click(function() {
+                  $(this).closest("tr").remove();
+              })
+      ).appendTo($(tr));
+      */
+      
+      // add the new row
+      $(tr).appendTo($('#tab_logic'));
+      
+      $(tr).find("td button.row-remove").on("click", function() {
+           $(this).closest("tr").remove();
+      });
+});
+
+
+
+
+  // Sortable Code
+  var fixHelperModified = function(e, tr) {
+      var $originals = tr.children();
+      var $helper = tr.clone();
+  
+      $helper.children().each(function(index) {
+          $(this).width($originals.eq(index).width())
+      });
+      
+      return $helper;
+  };
+
+  $(".table-sortable tbody").sortable({
+      helper: fixHelperModified      
+  }).disableSelection();
+
+  $(".table-sortable thead").disableSelection();
+
+
+
+  $("#add_row").trigger("click");
+    
   });
   
